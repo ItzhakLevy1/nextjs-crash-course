6a5eab462a9e5a22f16547a00e45a0993185daca
Itzhak Levy <itzhaklevydev@gmail.com>
2025-12-28 11:59:12 +0200

    Change the fonts in app\layout.tsx and the meta data content
    - Install npx shadcn@latest add @react-bits/LightRays-JS-CSS and shadcn
    - Create components\LightRays.tsx
    - Edit the app\page.tsx and app\layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 const page = () => {
   return (
-    <div>Welcome to Next.js 16 !</div>
-  )
-}
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackatons, Meetups, and Confrences, All in One Place
+      </p>
+    </section>
+  );
+};
 
-export default page
+export default page;
